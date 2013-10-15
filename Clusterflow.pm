d03736471ceff91e968dcf9bef335cf91b2be2f4
Phil Ewels <phil.ewels@scilifelab.se>
2013-10-15 16:38:49 +0000

    Finished code for downloads. Wrote download module, sra dump and fastQC

--- Clusterflow.pm
+++ Clusterflow.pm
@@ -14,17 +14,27 @@ sub load_runfile_params {
 	unless (defined $prev_job_id && length($prev_job_id) > 0) {
 		die "Previous job ID not specified\n";
 	}
+	unless ($parameters) {
+		$parameters = '';
+	}
+	
+	my $num_input_files = 0;
 	
 	warn "\nRun File:\t\t$runfile\nJob ID:\t\t\t$job_id\nPrevious Job ID:\t$prev_job_id\nParameters:\t\t$parameters\n\n";
 
 	open (RUN,$runfile) or die "Can't read $runfile: $!";
 
 	my @files;
+	my %config;
 	my $comment_block = 0;
 	while(<RUN>){
+	
+		# clean up line
 		chomp;
 		s/\n//;
 		s/\r//;
+		
+		# Ignore comment blocks
 		if($_ =~ /^\/\*/){
 			$comment_block = 1;
 			next;
@@ -33,17 +43,32 @@ sub load_runfile_params {
 			$comment_block = 0;
 			next;
 		}
+		
+		# Get config variables
+		if($_ =~ /^\@/ && !$comment_block){
+			my @sections = split(/:/, $_, 2);
+			$config{substr($sections[0], 1)} = $sections[1];
+		}
+		
+		# Get files
 		if($_ =~ /^[^@#]/ && !$comment_block){
 			my @sections = split(/\t/, $_, 2);
 			if($sections[0] eq $prev_job_id){
 				push(@files, $sections[1]);
+				$num_input_files++;
 			}
 		}
 	}
 	
 	close(RUN);
 	
-	return (\@files, $runfile, $job_id, $prev_job_id, $parameters);
+	# If we don't have any input files, bail now
+	if($num_input_files == 0){
+		print "Error - no input files found.\n";
+		exit;
+	}
+	
+	return (\@files, $runfile, $job_id, $prev_job_id, $parameters, \%config);
 }
 
 
