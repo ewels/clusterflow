556aa3116986803246730db7da0544294df07a8b
Phil Ewels <phil.ewels@scilifelab.se>
2013-11-07 14:32:20 +0000

    Rewrote module load functionality. Wrote --create_config functionality.

--- source/CF/Constants.pm
+++ source/CF/Constants.pm
@@ -35,6 +35,15 @@ parse_conf_file ();
 
 sub parse_conf_file {
 	
+	# Warn user if we don't have a home directory config file
+	# (probably means no notifications)
+	unless(-e "$homedir/clusterflow/clusterflow.config"){
+		warn "\nYou don't seem to have a config file set up in your
+home directory! It's a good idea to set one up, so that
+you can get e-mailed notifications from Cluster Flow.
+To create one, run cf --make_config\n\n\n";
+	}
+	
 	
 	# Read global config variables in. Do in order so that local prefs overwrite.
 	
@@ -242,7 +251,10 @@ AVAILABLE FLAGS
 		
 	--dryrun
 		Prints jobs to terminal instead of submitting them to the cluster
-		
+	
+	--make_config
+		Interactive prompt to generate a personalised CF config file
+	
 	--version
 		Print version of Cluster Flow installed
 		
@@ -259,6 +271,178 @@ SEE ALSO
 EOT
 	
 	return ($help);
+
+}
+
+
+
+
+# Function to run interactive shell prompt to generate a config file for first run
+sub clusterflow_make_config{
+	
+	my $fn = $homedir."/clusterflow/clusterflow.config";
+	
+	print "\n\nCluster Flow Config Generator\n==============================\nRunning Cluster Flow version $CF_VERSION\n";
+	print "This mode will generate a personalised Cluster Flow config file for you \nin your home directory: ~/clusterflow/clusterflow.config\n\n";
+	
+	if(-e $fn){
+		print "### WARNING ###\n$fn already exists!\nThis script will overwrite that file. Do you want to continue? (y/n)\n\n";
+		while (my $continue = <STDIN>){
+			chomp ($continue);
+			if ($continue =~ /^n(o)?/i){
+				print "\nProbably wise.. See the manual for more information about how the config file works.\n\n";
+				exit;
+			} elsif($continue =~ /^y(es)?/i){
+				print "\nOk, no problem.. I'll wipe it when we get to the end.\n\n";
+				last;
+			} else {
+				print "\nSorry, I didn't understand that.\nCould you try again please? (y/n)\n\n";
+			}
+		}
+	}
+	
+	my $config = "/*
+Clusterflow Config
+-------------------
+Default static variables for clusterflow.
+Syntax - \@key:value
+These will overwrite any with the same name in the centralised config file
+-------------------
+*/\n\n";
+	
+	my $email;
+	print "Right, let's get started! First off - what is your e-mail address?\nThis will be used for sending notifications.\n\n";
+	while ($email = <STDIN>){
+		chomp ($email);
+		if($email =~ /^\w[\w\.\-]*\w\@\w[\w\.\-]*\w(\.\w{2,4})$/){
+			print "\nGreat! That looks good..\n\n";
+			last;
+		} else {
+			print "\nHmm, that e-mail address looked a little odd.\nAre you sure you typed it in correctly? (y/n)\n\n";
+			my $invalidemail = <STDIN>;
+			chomp($invalidemail);
+			if($invalidemail =~ /^y(es)?/i){
+				print "\nFair enough!\n\n";
+				last;
+			} else {
+				print "\nNo problem.. Please try it again..\n\n";
+			}
+		}
+	}
+	$config .= "\@email	$email\n";
+	
+	my ($notify_complete, $notify_run, $notify_success, $notify_error, $notify_abort);
+	
+	print "Would you like to receive a notification when a pipeline is completed?
+The e-mail tells you the pipeline that has finished, the working directory
+for that pipeline, a list of Cluster Flow highlight notifications (typically
+whether each step in the pipeline ran successfully for each file) and then the
+log file output for each file. These notifications are recommended (y/n)\n\n";
+	while ($notify_complete = <STDIN>){
+		chomp ($notify_complete);
+		if($notify_complete =~ /^y(es)?/i){
+			print "\nGreat!\n\n";
+			$config .= "\@notification	complete\n";
+			last;
+		} elsif ($notify_complete =~ /^n(o)?/i){
+			print "\nOk, fair enough..\n\n";
+			last;
+		} else {
+			print "\nSorry, I didn't understand that.\nCould you try again please? (y/n)\n\n";
+		}
+	}
+	
+	print "Would you like to receive a notification when each run is completed?
+Usually a run is the processing of one input file. The e-mail tells you the
+name of the run that has finished, its pipeline and the working directory.
+It includes a list of Cluster Flow highlight notifications (typically
+whether each step in the pipeline ran successfully) and then the log file output.
+These notifications are recommended for those who like to keep a close eye on
+their processing (y/n)\n\n";
+	while ($notify_run = <STDIN>){
+		chomp ($notify_run);
+		if($notify_run =~ /^y(es)?/i){
+			print "\nGreat!\n\n";
+			$config .= "\@notification	run\n";
+			last;
+		} elsif ($notify_run =~ /^n(o)?/i){
+			print "\nOk, sounds good..\n\n";
+			last;
+		} else {
+			print "\nSorry, I didn't understand that.\nCould you try again please? (y/n)\n\n";
+		}
+	}
+	
+	print "Would you like to receive a notification when step of each run is
+completed? This will be a GRID Engine notice for every qsub job. These notifications
+are not recommended as a typicaly Cluster Flow run can flood your inbox with hundreds
+of such e-mails. Would you like to receive them? (y/n)\n\n";
+	while ($notify_success = <STDIN>){
+		chomp ($notify_success);
+		if($notify_success =~ /^y(es)?/i){
+			print "\nFair enough, you were warned!\n\n";
+			$config .= "\@notification	success\n";
+			last;
+		} elsif ($notify_success =~ /^n(o)?/i){
+			print "\nProbably sensible..\n\n";
+			last;
+		} else {
+			print "\nSorry, I didn't understand that.\nCould you try again please? (y/n)\n\n";
+		}
+	}
+	
+	print "Would you like to receive a notification when a GRID Engine
+job exits in an error state? This is a fairly crude way to be notified
+when something in the pipeline has gone badly wrong. Note that most
+Cluster Flow modules exit with a success state even if they failed.
+You're unlikely to get many if any, so they're recommended.
+Would you like to receive these notifications? (y/n)\n\n";
+	while ($notify_error = <STDIN>){
+		chomp ($notify_error);
+		if($notify_error =~ /^y(es)?/i){
+			print "\nSounds good!\n\n";
+			$config .= "\@notification	error\n";
+			last;
+		} elsif ($notify_error =~ /^n(o)?/i){
+			print "\nFair enough..\n\n";
+			last;
+		} else {
+			print "\nSorry, I didn't understand that.\nCould you try again please? (y/n)\n\n";
+		}
+	}
+	
+	print "Ok, last one. Would you like to receive a notification 
+when a GRID Engine job exits in an abort state? This typically only
+happens when you or an administrator kills your cluster jobs using
+qdel. You're unlikely to get many of these, so they're recommended.
+Would you like to receive these notifications? (y/n)\n\n";
+	while ($notify_abort = <STDIN>){
+		chomp ($notify_abort);
+		if($notify_abort =~ /^y(es)?/i){
+			print "\nSounds good!\n\n";
+			$config .= "\@notification	abort\n";
+			last;
+		} elsif ($notify_abort =~ /^n(o)?/i){
+			print "\nFair enough..\n\n";
+			last;
+		} else {
+			print "\nSorry, I didn't understand that.\nCould you try again please? (y/n)\n\n";
+		}
+	}
+	$config .= "\n\n\n";
+	
+	
+	print "\n\nGreat, that's it! The following config file will be created:\n\n$config\n";
+	
+	print "\nRemember that you can add further settings to your
+personalised config file - see the Cluster Flow manual
+for further information.\n\n\n";
+	
+	
+	open (OUT, '>', $fn) or die "Can't read $fn: $!";
+	print OUT $config;
+	close OUT;
+	
 
 }
 
