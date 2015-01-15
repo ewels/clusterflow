733c64691087a4799482deae4ab32f25e8c371b9
s-andrews <simon@proteo.me.uk>
2015-01-15 09:34:10 +0000

    Remove odd plus symbols from the Constants module for the STAR components

--- source/CF/Constants.pm
+++ source/CF/Constants.pm
@@ -211,10 +211,10 @@ sub parse_genomes_file {
 						$BOWTIE2_SPECIES{$key} = $species;
 						$BOWTIE2_ASSEMBLIES{$key} = $assembly;
 					} elsif($path_type eq 'star_path'){
-+						$STAR_PATH_CONFIGS{$key} = $genome_file;
-+						$STAR_PATHS{$key} = $path;
-+						$STAR_SPECIES{$key} = $species;
-+						$STAR_ASSEMBLIES{$key} = $assembly;
+						$STAR_PATH_CONFIGS{$key} = $genome_file;
+						$STAR_PATHS{$key} = $path;
+						$STAR_SPECIES{$key} = $species;
+						$STAR_ASSEMBLIES{$key} = $assembly;
 					} elsif($path_type eq 'gtf_path'){
 						$GTF_PATH_CONFIGS{$key} = $genome_file;
 						$GTF_PATHS{$key} = $path;
