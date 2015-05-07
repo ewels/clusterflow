2e0f9459549a74784e3ac57116db1bf41191e4f1
Phil Ewels <phil.ewels@scilifelab.se>
2015-05-07 21:22:06 +0200

    heads, shoulders knees and toes, knees and toes

--- docs/_site/includes/header.php
+++ docs/_site/includes/header.php
@@ -51,7 +51,7 @@
 			<a href="http://www.babraham.ac.uk" target="_blank"><img src="_site/img/Babraham_logo.png" title="Babraham Institute"></a>
 			<a href="http://www.scilifelab.se" target="_blank"><img src="_site/img/SciLifeLab_logo.png" title="SciLifeLab"></a>
 		</p>
-		<p>This documentation is written using <a href="<?php echo $source; ?>.md">markdown</a> and included with the Cluster Flow source code.</p>
+		<p>This documentation is <a href="<?php echo basename($source); ?>" title="View the markdown source for this page">written using markdown</a> and is included with the Cluster Flow source code.</p>
 	</section>
 </header>
 
