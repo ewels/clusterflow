bd7258c41d4ad431fdb30a35dbcd5987f4f67a7e
Phil Ewels <phil.ewels@scilifelab.se>
2015-05-11 16:45:36 +0200

    First draft of new website homepage

---
+++ docs/_site/homepage/index.php
@@ -0,0 +1,93 @@
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Cluster Flow</title>
+    <meta name="description" content="A simple and flexible bioinformatics pipeline tool">
+    <meta name="author" content="Phil Ewels">
+
+    <!-- Bootstrap -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Google reCaptcha -->
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+
+    <!-- Custom Styles -->
+    <link href="styles.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+</html>
+
+<body>
+
+<header>
+  <div class="header-panel">
+      <h1><img src="img/Cluster_Flow.png" alt="Cluster Flow"></h1>
+      <h2 class="hidden-xs"><small>A simple and flexible bioinformatics pipeline tool</small></h2>
+  </div>
+</header>
+
+<main>
+  <div class="container">
+    <div class="intro text-center">
+      <p class="lead">Cluster Flow is designed to be quick and easy to install,
+        with flexible configuration and simple customization.</p>
+      <p>It's easy enough to set up and use for non-bioinformaticians (given a basic
+        knowledge of the command line), and it's simplicity makes it great for low
+        to medium throughput analyses.</p>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-sm-4">
+        <a class="btn btn-lg btn-block btn-default" href="0.4">
+          <i class="fa fa-book fa-lg"></i>
+          Read the Docs
+        </a>
+        <span class="visible-xs">&nbsp;</span>
+      </div>
+      <div class="col-sm-4">
+        <a class="btn btn-lg btn-block btn-primary" href="https://github.com/ewels/clusterflow/releases">
+          <i class="fa fa-download fa-lg"></i>
+          Download
+        </a>
+        <span class="visible-xs">&nbsp;</span>
+      </div>
+      <div class="col-sm-4">
+        <a class="btn btn-lg btn-block btn-default" href="0.4/demo">
+          <i class="fa fa-life-ring fa-lg"></i>
+          Try the Demo
+        </a>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-sm-6">
+        <h2>Development Version</h2>
+        <p>Cluster Flow is developed on <a href="https://github.com/ewels/clusterflow">GitHub</a>, where you can get the current development version. You can see the development version <a href="0.4devel">documentation here</a>.</p>
+      </div>
+      <div class="col-sm-6 text-right">
+        <h2>Contributing</h2>
+        <p>Cluster Flow is an open-source project, with a number of <a href="https://github.com/ewels/clusterflow/graphs/contributors">contributors</a>.
+          If you would like to add a module or pipeline, please see the <a href="https://github.com/ewels/clusterflow/blob/master/CONTRIBUTING.md">instructions</a>.</p>
+      </div>
+    </div>
+    <hr>
+  </div>
+</main>
+<footer class="container">
+  Cluster Flow was written by <a href="http://phil.ewels.co.uk" target="_blank">Phil Ewels</a> whilst working at the <a href="http://www.bioinformatics.babraham.ac.uk/" target="_blank">Babraham Institute</a> and now the <a href="http://www.scilifelab.se/facilities/genomics-applications/" target="_blank">Science for Life Laboratory</a>.
+</footer>
+</body>
+</html>
