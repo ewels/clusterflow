0a9b086f517b8065a1c57f17ce0d534a4b9e43fa
FelixKrueger <felix.krueger@babraham.ac.uk>
2016-07-20 13:47:46 +0100

    Fixed an eq instead of ==

--- modules/bismark_align.cfmod.pl
+++ modules/bismark_align.cfmod.pl
@@ -120,7 +120,7 @@ if($se_files && scalar(@$se_files) > 0){
 		$output_fn =~ s/(\.fastq\.gz|\.fq\.gz|\.fastq|\.fq)$//; # attempting to remove fastq.gz etc to make filename a little shorter 05 02 2016. Felix
 		$output_fn .= "_".$cf{config}{genome};
 		my $basename = $output_fn;
-		if($bowtie == '--bowtie2'){
+		if($bowtie eq '--bowtie2'){
 		    $output_fn .= "_bismark_bt2.bam";
 		    $basename .= "_bismark_bt2";
 		} else {
